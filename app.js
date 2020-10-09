536e81afdd8c7055f570c48cb9db4e075f331b7e
STAM0283 <aminestambouli00780@gemail.com>
2020-10-09 14:32:25 +0200

    Change from route

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.post("/contact", (req, res, next) => {
   });
 
   let info = transporter.sendMail({
-    from: "aminestambouli00780@gmail.com", 
+    from: req.body.email, 
     to: "aminestambouli00780@gmail.com",
     firstName: req.body.firstName, 
     lastName: req.body.lastName,
