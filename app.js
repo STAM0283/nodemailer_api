9591e812a605253a38db08aa2fa201fb2c6cc96e
STAM0283 <aminestambouli00780@gemail.com>
2020-10-09 22:30:12 +0200

    correction contact.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const rateLimit = require("express-rate-limit");
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -17,6 +16,8 @@ app.use((req, res, next) => {
   next();
 });
 
+const rateLimit = require("express-rate-limit");
+
 const limiter = rateLimit({
   windowMs: 3 * 60 * 1000,
   max: 30, // per IP
@@ -26,53 +27,92 @@ app.use(limiter);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post("/contact", (req, res, next) => {
-  // const output = `
-  //     <p>Vous avez un nouveau message</p>
-  //     <h3>Détails du contact</h3>
-  //     <ul>
-  //        <li>Nom : ${req.body.firstName}</li>
-  //        <li>Prénom : ${req.body.lastName}</li>
-  //        <li>E-mail: ${req.body.email}</li>
-  //     </ul>
-  //     <h3>Message :</h3>
-  //     <p>${req.body.message}</p>
-  // `;
-  let transporter = nodemailer.createTransport({
+app.post("/send-email", function (req, res, next) {
+  const transporter = nodeMailer.createTransport({
     host: "smtp.gmail.com",
     service: "gmail",
-    port: 465,
-    secure: true, 
     auth: {
-      user: process.env.EMAIL, 
-      pass: process.env.PASSWORD
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
     },
+    port: 465,
+    secure: true,
   });
 
-  let info = transporter.sendMail({
-    from: process.env.EMAIL, 
+  const mailOptions = {
+    from: req.body.email,
     to: process.env.EMAIL,
-    firstName: req.body.firstName, 
-    lastName: req.body.lastName,
-    email: req.body.email, 
-    message: req.body.message, 
+    subject: req.body.message,
     html:
-      req.body.firstName +
+      req.body.firtsName +
       " (" +
       req.body.lastName +
       " (" +
       req.body.email +
       ") " +
       " send this message : " +
-      req.body.message, 
-  }, (err) => {
-    if(err){
-      console.log("il ya eu une erreur dans l'envois du mail")
+      req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.status(200).json({
+        success: true,
+        messages: "Email Sent",
+      });
     }
-    res.send("Email a été envoyé avec succés")
   });
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-})
+});
+
+// app.post("/contact", (req, res, next) => {
+//   // const output = `
+//   //     <p>Vous avez un nouveau message</p>
+//   //     <h3>Détails du contact</h3>
+//   //     <ul>
+//   //        <li>Nom : ${req.body.firstName}</li>
+//   //        <li>Prénom : ${req.body.lastName}</li>
+//   //        <li>E-mail: ${req.body.email}</li>
+//   //     </ul>
+//   //     <h3>Message :</h3>
+//   //     <p>${req.body.message}</p>
+//   // `;
+//   let transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     service: "gmail",
+//     port: 465,
+//     secure: true, 
+//     auth: {
+//       user: process.env.EMAIL, 
+//       pass: process.env.PASSWORD
+//     },
+//   });
+
+//   let info = transporter.sendMail({
+//     from: process.env.EMAIL, 
+//     to: process.env.EMAIL,
+//     firstName: req.body.firstName, 
+//     lastName: req.body.lastName,
+//     email: req.body.email, 
+//     message: req.body.message, 
+//     html:
+//       req.body.firstName +
+//       " (" +
+//       req.body.lastName +
+//       " (" +
+//       req.body.email +
+//       ") " +
+//       " send this message : " +
+//       req.body.message, 
+//   }, (err) => {
+//     if(err){
+//       console.log("il ya eu une erreur dans l'envois du mail")
+//     }
+//     res.send("Email a été envoyé avec succés")
+//   });
+//   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+// })
 
 
 
