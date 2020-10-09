8c3a6d2d1193c62f72d5a688092dcad6dd504d56
STAM0283 <aminestambouli00780@gemail.com>
2020-10-09 16:18:14 +0200

    Change app.js content

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
-const rateLimit = require("express-rate-limit");
+const express = require("express");
+const nodeMailer = require("nodemailer");
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
-const PORT = process.env.PORT || 5000;
-const app = express();
 
+const app = express();
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://curriculum-vitae-stambouli-amine.netlify.app");
@@ -16,6 +14,7 @@ app.use((req, res, next) => {
   );
   next();
 });
+const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 3 * 60 * 1000,
@@ -26,51 +25,122 @@ app.use(limiter);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post("/contact", (req, res, next) => {
-  const output = `
-      <p>Vous avez un nouveau message</p>
-      <h3>Détails du contact</h3>
-      <ul>
-         <li>Nom : ${req.body.firstName}</li>
-         <li>Prénom : ${req.body.lastName}</li>
-         <li>E-mail: ${req.body.email}</li>
-      </ul>
-      <h3>Message :</h3>
-      <p>${req.body.message}</p>
-  `;
-  let transporter = nodemailer.createTransport({
+app.post("/contact", function (req, res, next) {
+  const transporter = nodeMailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true, 
+    service: "gmail",
     auth: {
-      user: "aminestambouli00780@gmail.com", 
-      pass: "stam@00780",
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
     },
+    port: 465,
+    secure: true,
   });
 
-  let info = transporter.sendMail({
-    from: "aminestambouli00780@gmail.com", 
-    to: "aminestambouli00780@gmail.com",
-    firstName: req.body.firstName, 
-    lastName: req.body.lastName,
-    email: req.body.email, 
-    message: req.body.message, 
-    html: output, 
-  }, (err) => {
-    if(err){
-      console.log("il ya eu une erreur dans l'envois du mail")
+  const mailOptions = {
+    from: req.body.email,
+    to: process.env.EMAIL,
+    subject: req.body.message,
+    html:
+      req.body.firstName +
+      " (" +
+       req.body.lastName +
+       " (" +
+      req.body.email +
+      ") " +
+      " send this message : " +
+      req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Email Sent",
+      });
     }
-    res.send("Email a été envoyé avec succés")
   });
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-})
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  const port = process.env.PORT;
+  console.log("Server started at", port);
+});
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const nodemailer = require('nodemailer');
+// const rateLimit = require("express-rate-limit");
+// const dotenv = require("dotenv");
+// dotenv.config();
+// const PORT = process.env.PORT || 5000;
+// const app = express();
+
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://curriculum-vitae-stambouli-amine.netlify.app");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   next();
+// });
+
+// const limiter = rateLimit({
+//   windowMs: 3 * 60 * 1000,
+//   max: 30, // per IP
+// });
+
+// app.use(limiter);
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+// app.post("/contact", (req, res, next) => {
+//   const output = `
+//       <p>Vous avez un nouveau message</p>
+//       <h3>Détails du contact</h3>
+//       <ul>
+//          <li>Nom : ${req.body.firstName}</li>
+//          <li>Prénom : ${req.body.lastName}</li>
+//          <li>E-mail: ${req.body.email}</li>
+//       </ul>
+//       <h3>Message :</h3>
+//       <p>${req.body.message}</p>
+//   `;
+//   let transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true, 
+//     auth: {
+//       user: process.env.EMAIL, 
+//       pass: process.env.PASSWORD
+//     },
+//   });
+
+//   let info = transporter.sendMail({
+//     from: req.body.email, 
+//     to: process.env.EMAIL,
+//     firstName: req.body.firstName, 
+//     lastName: req.body.lastName,
+//     email: req.body.email, 
+//     message: req.body.message, 
+//     html: output, 
+//   }, (err) => {
+//     if(err){
+//       console.log("il ya eu une erreur dans l'envois du mail")
+//     }
+//     res.send("Email a été envoyé avec succés")
+//   });
+//   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+// })
 
 
 
 
-app.listen(PORT, (err) => {
-  if (err) {
-    throw new Error("Something bad happened...");
-  }
-  console.log(`Server is listening on ${PORT}`)
-})
+// app.listen(PORT, (err) => {
+//   if (err) {
+//     throw new Error("Something bad happened...");
+//   }
+//   console.log(`Server is listening on ${PORT}`)
+// })
