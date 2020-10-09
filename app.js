431d9d8562d7002089e5173069f3457b51df34d6
STAM0283 <aminestambouli00780@gemail.com>
2020-10-09 21:47:14 +0200

    Change info variable

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.post("/contact", (req, res, next) => {
   });
 
   let info = transporter.sendMail({
-    from: req.body.email, 
+    from: process.env.EMAIL, 
     to: process.env.EMAIL,
     firstName: req.body.firstName, 
     lastName: req.body.lastName,
