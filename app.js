bf06767a36d4520e7fd30d9bf6860a814304026c
STAM0283 <aminestambouli00780@gemail.com>
2020-10-10 11:49:48 +0200

    Correction

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
@@ -26,76 +27,48 @@ app.use(limiter);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post("/send-email", function (req, res, next) {
-  const transporter = nodeMailer.createTransport({
+
+app.post("/send-email", (req, res, next) => {
+  const output = `
+      <p>Vous avez un nouveau message</p>
+      <h3>Détails du contact</h3>
+      <ul>
+         <li>Nom : ${req.body.firstName}</li>
+         <li>Prénom : ${req.body.lastName}</li>
+         <li>E-mail: ${req.body.email}</li>
+      </ul>
+      <h3>Message :</h3>
+      <p>${req.body.message}</p>
+  `;
+  let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     service: "gmail",
+    port: 465,
+    secure: true, 
     auth: {
-      user: process.env.EMAIL,
+      user: process.env.EMAIL, 
       pass: process.env.PASSWORD,
     },
-    port: 465,
-    secure: true,
   });
 
-  const mailOptions = {
-    from: req.body.email,
+  let info = transporter.sendMail({
+    from: req.body.email, 
     to: process.env.EMAIL,
-    subject: req.body.message,
-    html:
-      req.body.firstName +
-      " (" +
-      req.body.lastName +
-      " (" +
-      req.body.email +
-      ") " +
-      " send this message : " +
-      req.body.message,
-  };
-
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.status(200).json({
-        success: true,
-        message: "Email Sent",
-      });
+    firstName: req.body.firstName, 
+    lastName: req.body.lastName,
+    email: req.body.email, 
+    message: req.body.message, 
+    html: output, 
+  }, (err) => {
+    if(err){
+      console.log("il ya eu une erreur dans l'envois du mail")
     }
+    res.send("Email a été envoyé avec succés")
   });
-});
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+})
 
 
-// app.post("/send-mail", (req, res, next) => {
-//   const output = `
-//       <p>Vous avez un nouveau message</p>
-//       <h3>Détails du contact</h3>
-//       <ul>
-//          <li>Nom : ${req.body.firstName}</li>
-//          <li>Prénom : ${req.body.lastName}</li>
-//          <li>E-mail: ${req.body.email}</li>
-//       </ul>
-//       <h3>Message :</h3>
-//       <p>${req.body.message}</p>
-//   `;
-
-
-//   let info = transporter.sendMail({
-//     from: "aminestambouli00780@gmail.com", 
-//     to: "aminestambouli00780@gmail.com",
-//     firstName: req.body.firstName, 
-//     lastName: req.body.lastName,
-//     email: req.body.email, 
-//     message: req.body.message, 
-//     html: output, 
-//   }, (err) => {
-//     if(err){
-//       console.log("il ya eu une erreur dans l'envois du mail")
-//     }
-//     res.send("Email a été envoyé avec succés")
-//   });
-//   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-// })
 
 
 app.listen(PORT, (err) => {
